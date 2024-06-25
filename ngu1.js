cb87d5ccc6b3b918c93b415cf62406b782473c01
jloshngu <jlosh@ngu.edu>
2024-06-25 12:12:04 -0400

    Update ngu1.js

---
+++ ngu1.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    let customCssLink = $("<link>", {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://cdn.jsdelivr.net/gh/jloshngu/jloshngu.github.io@main/ngu1.css"
+    });
+    customCssLink.appendTo('head');
+});
